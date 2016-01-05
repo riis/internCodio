473e892f65026facfe12da09a90ef86f615ae76b
Tom Kocik <tkocik@RIIS-MBPRO-16.local>
2016-01-05 10:03:17 -0500

    Testing MVVM. I'm not sure this will work?

--- 5-CropCompare/CropCompare/app/src/main/java/com/riis/cropcompare/activity/ResultsActivity.java
+++ 5-CropCompare/CropCompare/app/src/main/java/com/riis/cropcompare/activity/ResultsActivity.java
@@ -2,27 +2,33 @@ package com.riis.cropcompare.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.riis.cropcompare.R;
+import com.riis.cropcompare.databinding.ActivityResultsBinding;
 import com.riis.cropcompare.model.CropDetailResponse;
 import com.riis.cropcompare.model.CropResults;
 import com.riis.cropcompare.model.HandleResponseInterface;
 import com.riis.cropcompare.model.Vault;
 import com.riis.cropcompare.util.GetCropDetailsTask;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ResultsActivity extends Activity implements HandleResponseInterface
 {
+    private final Map<String, String> mCosts = new HashMap<>();
+
     private CropResults mCropResults;
     private boolean mPriceFound = false;
     private boolean mYieldFound = false;
-    private int mAcreage;
     private String mCropSelected;
-    private TextView mCostTextView;
-    private TextView mPriceTextView;
-    private TextView mTotalTextView;
-    private TextView mYieldTextView;
+//    private TextView mCostTextView;
+//    private TextView mPriceTextView;
+//    private TextView mTotalTextView;
+//    private TextView mYieldTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,20 +36,22 @@ public class ResultsActivity extends Activity implements HandleResponseInterface
         setContentView(R.layout.activity_results);
 
         Intent intent = getIntent();
-        mAcreage = Integer.parseInt(intent.getStringExtra("acreage"));
+        int acreage = Integer.parseInt(intent.getStringExtra("acreage"));
         String stateSelected = intent.getStringExtra("stateSelected");
         mCropSelected = intent.getStringExtra("cropSelected");
 
-        mPriceTextView = (TextView)findViewById(R.id.priceResultTextView);
-        mYieldTextView = (TextView)findViewById(R.id.yieldResultTextView);
-        mTotalTextView = (TextView)findViewById(R.id.totalResultTextView);
-        mCostTextView = (TextView)findViewById(R.id.costResultTextView);
+//        mPriceTextView = (TextView)findViewById(R.id.priceResultTextView);
+//        mYieldTextView = (TextView)findViewById(R.id.yieldResultTextView);
+//        mTotalTextView = (TextView)findViewById(R.id.totalResultTextView);
+//        mCostTextView = (TextView)findViewById(R.id.costResultTextView);
 
         mCropResults = new CropResults();
+        mCropResults.setAcreage(acreage);
+
+        ActivityResultsBinding activityResultsBinding = DataBindingUtil.setContentView(this, R.layout.activity_results);
+        activityResultsBinding.setVariable(com.riis.cropcompare.BR.cropResultModel, mCropResults);
 
         setUpCosts();
-
-        //mProgressBar.setVisibility(View.VISIBLE);
 
         new GetCropDetailsTask(this, true).execute(Vault.getCropYieldUrl(stateSelected, mCropSelected));
         new GetCropDetailsTask(this, true).execute(Vault.getCropPriceUrl(stateSelected, mCropSelected));
@@ -78,30 +86,17 @@ public class ResultsActivity extends Activity implements HandleResponseInterface
     }
 
     private void setResultText() {
-        String placeholder = "%.02f";
-        float costOfCrop = 0;
-        boolean costFound = false;
         try
         {
-            costOfCrop = Float.parseFloat(mCosts.get(mCropSelected.toUpperCase()));
-            costFound = true;
+            mCropResults.setCropCost(Float.parseFloat(mCosts.get(mCropSelected.toUpperCase())));
         }
         catch(Exception e) {
             e.printStackTrace();
         }
 
-        mYieldTextView.setText(String.valueOf(mYieldData * mAcreage));
-        mPriceTextView.setText(String.format(placeholder, mPriceData));
-        if(costFound)
-        {
-            mCostTextView.setText("$" + String.format(placeholder, costOfCrop * mAcreage));
-        }
-        else
-        {
-            mCostTextView.setText("N/A");
-        }
-
-        mTotalTextView.setText("$" + String.format(placeholder,
-                (mPriceData * mYieldData * mAcreage) - (costOfCrop * mAcreage)));
+//        mYieldTextView.setText(mCropResults.getYieldTotal());
+//        mPriceTextView.setText(mCropResults.getPricePerBuString());
+//        mCostTextView.setText(mCropResults.getCropEstimate());
+//        mTotalTextView.setText(mCropResults.getTotalString());
     }
 }
--- 5-CropCompare/CropCompare/app/src/main/java/com/riis/cropcompare/model/CropResults.java
+++ 5-CropCompare/CropCompare/app/src/main/java/com/riis/cropcompare/model/CropResults.java
@@ -1,21 +1,20 @@
 package com.riis.cropcompare.model;
 
+import android.databinding.Bindable;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class CropResults {
+    private int mAcreage;
+    private float mCropCost = 0f;
     private float mYield;
     private float mPricePerBU;
     private float mEstimatedCost;
     private float mTotal;
 
-    public float getYield() {
-        return mYield;
-    }
-
     public void setYield(float yield) {
         mYield = yield;
-    }
-
-    public float getPricePerBU() {
-        return mPricePerBU;
     }
 
     public void setPricePerBU(float pricePerBU) {
@@ -26,15 +25,44 @@ public class CropResults {
         return mEstimatedCost;
     }
 
-    public void setEstimatedCost(float estimatedCost) {
-        mEstimatedCost = estimatedCost;
-    }
-
     public float getTotal() {
         return mTotal;
     }
 
-    public void setTotal(float total) {
-        mTotal = total;
+    public void setAcreage(int acreage)
+    {
+        mAcreage = acreage;
+    }
+
+    public void setCropCost(float cropCost)
+    {
+        mCropCost = cropCost;
+    }
+
+    public String getCropEstimate()
+    {
+        if (mCropCost == 0)
+        {
+            return "N/A";
+        }
+
+        return NumberFormat.getCurrencyInstance(Locale.US).format(mCropCost * mAcreage);
+    }
+
+    public String getPricePerBuString()
+    {
+        return String.valueOf(mPricePerBU);
+    }
+
+    @Bindable
+    public String getYieldTotal()
+    {
+        return String.valueOf(mAcreage * mYield);
+    }
+
+    public String getTotalString()
+    {
+        mTotal = (mPricePerBU * mYield * mAcreage) - (mCropCost * mAcreage);
+        return NumberFormat.getCurrencyInstance(Locale.US).format(mTotal);
     }
 }
