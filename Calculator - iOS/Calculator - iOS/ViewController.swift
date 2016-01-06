f77b8888796f43dfc9601e356ae7075a7817c042
Tom Kocik <tkocik@RIIS-MBPRO-16.local>
2016-01-06 10:55:08 -0500

    Calculator for iOS init commit

---
+++ Calculator - iOS/Calculator - iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator - iOS
+//
+//  Created by Tom Kocik on 1/6/16.
+//  Copyright © 2016 Tom Kocik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
